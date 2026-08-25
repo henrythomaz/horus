dbe0149284d2892b9dc6e7675689106c7e1a8a8a
henrythomaz <henrythcampos@gmail.com>
2026-08-25 08:13:17 -0400

    feat(main): Improving Neural Network.

--- mvp/train.py
+++ mvp/train.py
@@ -4,10 +4,10 @@ model = YOLO("yolo11n.pt")
 
 model.train(
     data="/home/henry/projetos/horus/datasets/DATASET.yolov8/data.yaml",
-    epochs=10,
-    imgsz=320,
+    epochs=50,
+    imgsz=640,
     batch=4,
     device="cpu",
     project="runs",
-    name="horus_mvp"
+    name="horus_640_50"
 )
